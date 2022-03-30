16a836166f2f13410335e055c2173a4ef4e14b05
Andrey Kachkin <8695505@gmail.com>
2022-03-30 03:03:54 +0300

    added checking address in serviced areas

--- app/lib/service-areas.ts
+++ app/lib/service-areas.ts
@@ -1,7 +1,12 @@
-import { FeatureCollection } from 'geojson';
+import { FeatureCollection, Polygon, Position, Feature } from 'geojson';
+import pointInPolygon from '@turf/boolean-point-in-polygon';
 import { getGeoJson } from '../orm/service-areas';
 
 export function findServiceArea(lat: number, lng: number) {
-  const geoJson = getGeoJson() as FeatureCollection;
-  return null;
+  const { features } = getGeoJson() as FeatureCollection;
+  
+  const district: Feature = features
+    .find((feature) => pointInPolygon([lng, lat] as Position, feature.geometry as Polygon))
+
+  return district ? district.properties.Name : null;
 }
